391c7baa3e3940936634a19d9e35c1996e54aec3
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-07-28 17:41:13 +0300

    added App.css styling

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,12 @@
 import React from "react";
 import BotCard from "./BotCollection";
 
-const BotCollection = ({bots,onEnlist}) => {
+const BotCollection = ({bots, onEnlist}) => {
   return (
     <div className="bot-collection">
       <h2>All Bots</h2>
       <div className="bot-cards">
-        {bots.map((bot)=>(
+        {bots.map((bot) => (
           <BotCard key={bot.id} bot={bot} onEnlist={()=> onEnlist(bot)} />
         ))}
       </div>
