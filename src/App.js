0c4726c710f03379406ef332e83ce737acc615cb
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-07-28 14:43:32 +0300

    set basic components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import BotCollection from './components/BotCollection';
+import YourBotArmy from './components/YourBotArmy';
 
+function App (){
+  return (
+    <div className='App'>
+      <h1>Bot Battlr</h1>
+      <div className='container'>
+        <YourBotArmy/>
+           
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
 
 
 
