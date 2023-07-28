3cab0055182b93f46a151b0a1247e00af4e732d2
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-07-28 16:55:17 +0300

    Fetch data from the server and render BotCard components in BotCollection.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import BotCollection from './components/BotCollection';
 import YourBotArmy from './components/YourBotArmy';
 
 function App (){
+  const [enlistedBots, setEnlistedBots] = useState([]);
+  const [bots, setBots] = useState([]);
+
+  useEffect (() => {
+    fetch("http://localhost:8001/bots")
+    .then((response) => response.json())
+    .then((data) => setBots(data))
+    .catch((error) => console.error('Error fetching data:',error));
+
+  }, []);
   return (
     <div className='App'>
       <h1>Bot Battlr</h1>
