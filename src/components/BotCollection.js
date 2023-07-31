b8294695cebef8fe80d94d3d2602d3c57b1004ac
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-07-31 18:34:39 +0300

    sort bots by damage, health, armor

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import BotCard from './BotCard';
+import SortBar from './SortBar';
 
 const BotCollection = ({ bots, onEnlist }) => {
+  const [sortedBots, setSortedBots] = useState([]);
+  const [sortOption, setSortOption] = useState(null);
+
+  useEffect(() => {
+    // display the bots as they are without sorting
+    setSortedBots(bots);
+  }, [bots]);
+
+  const handleSort = (option) => {
+   
+    if (sortOption === option) {
+      setSortedBots([...sortedBots.reverse()]);
+    } else {
+      setSortedBots([...bots.sort((a, b) => b[option] - a[option])]);
+    }
+    
+    setSortOption(option);
+  };
 
   return (
     <div className="bot-collection">
       <h2>All Bots</h2>
+      <SortBar onSort={handleSort} />
       <div className="bot-cards">
-        {bots.map((bot)=> (
-          <BotCard key={bot.id} bot={bot} onEnlist={()=> onEnlist(bot)} />
+        {sortedBots.map((bot) => (
+          <BotCard key={bot.id} bot={bot} onEnlist={() => onEnlist(bot)} />
         ))}
       </div>
     </div>
