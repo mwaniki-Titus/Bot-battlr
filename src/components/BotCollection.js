7e7068fa692607bd075eecedac1604eb4c06b09a
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-07-31 09:27:12 +0300

    deployment of the project

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React from "react";
 import BotCard from './BotCard';
 
 const BotCollection = ({ bots, onEnlist }) => {
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,4 +1,4 @@
-import React, {usestate} from "react";
+import React from "react";
 import BotCard from "./BotCard";
 
 const YourBotArmy =({enlistedBots, onRealease, onDischarge}) => {
